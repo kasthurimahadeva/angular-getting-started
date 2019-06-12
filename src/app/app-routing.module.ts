b4f6576c668ab80f8618be21e85181f65f028ea9
KasthuriMahadeva <kasthuri.14@cse.mrt.ac.lk>
2019-06-12 07:24:28 +0530

    guards and routings

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot([
+      {
+        path: 'welcome',
+        component: WelcomeComponent
+      },
+      {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full'
+      },
+      {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full'
+      }
+    ]),
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
